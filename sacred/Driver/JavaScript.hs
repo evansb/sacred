6117960480a1838bc33b17dc7d2bd66fab603f19
Evan Sebastian <evans@comp.nus.edu.sg>
2015-01-24 16:57:58 +0800

    Combine hashes

--- sacred/Driver/JavaScript.hs
+++ sacred/Driver/JavaScript.hs
@@ -3,7 +3,7 @@
 module Driver.JavaScript where
 
 import  Language.JavaScript.Parser
-import  Data.Hashable                   (hash)
+import  Data.Hashable                   (hash, hashWithSalt)
 import  Data.Text                       (pack)
 import  Data.IntervalMap.FingerTree     (point)
 import  Types
@@ -13,6 +13,13 @@ toPos (TokenPn _ ln col) = (ln, col)
 
 singletonNode :: String -> Maybe SourcePos -> STree
 singletonNode s p = SNode (hash s) [SLeaf (pack s) p]
+
+combineHashes :: [STree] -> Hash
+combineHashes [] = error "No."
+combineHashes (SNode h _ : xs) =
+        foldl (\acc x -> case x of
+                SNode h _ -> hashWithSalt acc h
+                _ -> error "No.") h xs
 
 toTree :: JSNode -> STree
 toTree n = case n of
@@ -28,4 +35,5 @@ toTree n = case n of
             JSOctal s -> singletonNode s p
             JSStringLiteral _ s -> singletonNode s p
             JSRegEx s -> singletonNode s p
-            JSArguments _ s _ -> SNode 0 (map toTree s)
+            JSArguments _ s _ -> let children = map toTree s in
+                    SNode (combineHashes children) children
